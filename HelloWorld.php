b15bc9ec2c59263c5b87e4b64bfb41d7bf6d0b44
Peter <s15ps2@abdn.ac.uk>
2017-12-02 20:16:49 +0000

    Added MySQL call
    MySQL call test

--- HelloWorld.php
+++ HelloWorld.php
@@ -5,10 +5,35 @@
 </HEAD>
 <BODY>
 
-Hello World
+Hello World<br>
 
 <?PHP
-echo "Hello World"
+	echo "Hello World<br><br>";
+
+	$servername = "eu-cdbr-azure-west-b.cloudapp.net";
+	$username = "peterszchechina";
+	$password = "Feynman6#!";
+	$dbname = "interview";
+
+	$connection = new mysql($servername, $username, $password, $dbname);
+
+	if ($connection->connect_error) {
+		die("Connection failed: " . $connection->connect_error);
+	}
+
+	$sql = "SELECT idInterview, interviewTitle, interviewText FROM interview";
+	$result = $connection->query($sql);
+
+	if($result->num_rows > 0){
+		echo "Interview, Title, Notes";
+		while($row = $result->fetch_assoc()){
+			echo $row["idInterview"]. $row["interviewTitle"]. $row["interviewText"]. "<br>";
+		}
+	} else {
+		echo "No records were returned.";
+	}
+	
+	$connection->close();
 ?>
 
 </BODY>
