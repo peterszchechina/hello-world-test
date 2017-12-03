a030e74d381c3265eeef90a28e5537a86376a953
Peter <s15ps2@abdn.ac.uk>
2017-12-03 15:08:29 +0000

    Changed connection and references

--- HelloWorld.php
+++ HelloWorld.php
@@ -16,12 +16,18 @@ Hello again<br>
 	$password = "Feynman6#!";
 	$dbname = "interviewx";
 
+	$conn = mysqli_init();
+	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+
+	if (mysqli_connect_errno($conn)) {
+		die('Failed to connect to MySQL: '.mysqli_connect_error());
+	}
 	//$servername = "eu-cdbr-azure-west-b.cloudapp.net";
 	//$username = "peterszchechina@interviewx";
 	//$password = "Feynman6#!";
 	//$dbname = "interview";
 
-	$connection = new mysql($servername, $username, $password, $dbname);
+	//$connection = new mysql($servername, $username, $password, $dbname);
 
 	//$connection=mysqli_init(); 
 	//[mysqli_ssl_set($connection, NULL, NULL, {ca-cert filename}, NULL, NULL);] 
@@ -31,23 +37,30 @@ Hello again<br>
 	//mysqli_ssl_set($connection, NULL, NULL, {ca-cert filename}, NULL, NULL); 
 	//mysqli_real_connect($connection, "interviewx.mysql.database.azure.com", "peterszchechina@interviewx", "Feynman6#!", "interviewx", 3306);
 	
-	if ($connection->connect_error) {
-		die("Connection failed: " . $connection->connect_error);
-	}
+	//if ($connection->connect_error) {
+	//	die("Connection failed: " . $connection->connect_error);
+	//}
 
 	$sql = "SELECT idInterview, interviewTitle, interviewText FROM interview";
-	$result = $connection->query($sql);
+	//$result = $conn->query($sql);
+	$res = mysqli_query($conn, $sql);
 
-	if($result->num_rows > 0){
-		echo "Interview, Title, Notes";
-		while($row = $result->fetch_assoc()){
-			echo $row["idInterview"]. $row["interviewTitle"]. $row["interviewText"]. "<br>";
-		}
-	} else {
-		echo "No records were returned.";
-	}
+	echo "Interview, Title, Notes";	
+	while ($row = mysqli_fetch_assoc($res)) {
+		var_dump($row);
+	}	
+
+	//if($result->num_rows > 0){
+	//	echo "Interview, Title, Notes";
+	//	while($row = $result->fetch_assoc()){
+	//		echo $row["idInterview"]. $row["interviewTitle"]. $row["interviewText"]. "<br>";
+	//	}
+	//} else {
+	//	echo "No records were returned.";
+	//}
 	
-	$connection->close();
+	//$conn->close();
+	mysqli_close($conn);
 ?>
 
 </BODY>
