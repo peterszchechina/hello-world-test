bb4405efaf763d707b4ffc760945054fda2c09f6
Peter <s15ps2@abdn.ac.uk>
2017-12-03 16:48:29 +0000

    SSL Disabled again no SSL set

--- HelloWorld.php
+++ HelloWorld.php
@@ -24,7 +24,7 @@ Hello Hello Hello<br>
 	
 	//mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ;
 	
-	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+	mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306);
 
 	if (mysqli_connect_errno($conn)) {
 		die('Failed to connect to MySQL: '.mysqli_connect_error());
