4bf90ce3cfdc79e7a0d5c13bf4680b80debd162a
Peter <s15ps2@abdn.ac.uk>
2017-12-03 13:04:29 +0000

    Edited Username

--- HelloWorld.php
+++ HelloWorld.php
@@ -11,10 +11,10 @@ Hello World<br>
 	echo "Hello World<br><br>";
 
 	$servername = "eu-cdbr-azure-west-b.cloudapp.net";
-	$username = "peterszchechina";
+	$username = "peterszchechina@interview";
 	$password = "Feynman6#!";
 	$dbname = "interview";
-
+	
 	$connection = new mysql($servername, $username, $password, $dbname);
 
 	if ($connection->connect_error) {
