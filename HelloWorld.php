6702b6ac04d51e47d0b21d25c861c09a3c14d43c
Peter <s15ps2@abdn.ac.uk>
2017-12-03 17:48:05 +0000

    With formatting.

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,15 +1,14 @@
 <!DOCTYPE HTML>
 <HTML>
 <HEAD>
-
+<link rel="stylesheet" type="text/css" href="HelloWorld.css">
 </HEAD>
 <BODY>
 
-Hello World Hello<br>
-Hello Hello Hello<br>
+<h1>Peter&quot;s PHP Page<br></h1>
 
 <?PHP
-	echo "Hello World Hello<br><br>";
+	echo "Hello World<br><br>";
 
 	$servername = "interviewx.mysql.database.azure.com";
 	$username = "peterszchechina@interviewx";
@@ -29,46 +28,20 @@ Hello Hello Hello<br>
 	if (mysqli_connect_errno($conn)) {
 		die('Failed to connect to MySQL: '.mysqli_connect_error());
 	}
-	//$servername = "eu-cdbr-azure-west-b.cloudapp.net";
-	//$username = "peterszchechina@interviewx";
-	//$password = "Feynman6#!";
-	//$dbname = "interview";
-
-	//$connection = new mysql($servername, $username, $password, $dbname);
-
-	//$connection=mysqli_init(); 
-	//[mysqli_ssl_set($connection, NULL, NULL, {ca-cert filename}, NULL, NULL);] 
-	//mysqli_real_connect($connection, $servername, {username@servername}, {your_password}, {your_database}, {your_port});
-
-	//$connection=mysqli_init(); 
-	//mysqli_ssl_set($connection, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-	//mysqli_real_connect($connection, "interviewx.mysql.database.azure.com", "peterszchechina@interviewx", "Feynman6#!", "interviewx", 3306);
-	
-	//if ($connection->connect_error) {
-	//	die("Connection failed: " . $connection->connect_error);
-	//}
 
 	$sql = "SELECT idInterview, interviewTitle, interviewText FROM interview";
-	//$result = $conn->query($sql);
+
 	$res = mysqli_query($conn, $sql);
 
-	echo "Interview, Title, Notes<br>";	
+	echo"<table>";
+	echo "<tr><th>Interview</th> <th>Title</th> <th>Notes</th></tr>";	
 	while ($row = mysqli_fetch_assoc($res)) {
 		//var_dump($row);
-		echo $row["idInterview"]. $row["interviewTitle"]. $row["interviewText"]. "<br>";
+		echo "<tr><td>". $row["idInterview"]. "</td><td>". $row["interviewTitle"]. "</td><td>".  $row["interviewText"]. "</td></tr>";
 
 	}	
+	echo"</table>";
 
-	//if($result->num_rows > 0){
-	//	echo "Interview, Title, Notes";
-	//	while($row = $result->fetch_assoc()){
-	//		echo $row["idInterview"]. $row["interviewTitle"]. $row["interviewText"]. "<br>";
-	//	}
-	//} else {
-	//	echo "No records were returned.";
-	//}
-	
-	//$conn->close();
 	mysqli_close($conn);
 ?>
 
